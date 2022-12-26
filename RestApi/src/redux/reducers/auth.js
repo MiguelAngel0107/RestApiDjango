71b64236fabfde5ceb6da06dfd214e04d37bc0fe
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2022-12-26 11:03:34 -0600

    update

--- RestApi/src/redux/reducers/auth.js
+++ RestApi/src/redux/reducers/auth.js
@@ -15,80 +15,18 @@ export default function Auth(state = initialState, action) {
     const { type, payload } = action;
 
     switch(type) {
-        case SET_AUTH_LOADING:
-            return {
-                ...state,
-                loading: true
-            }
-        case REMOVE_AUTH_LOADING:
-            return {
-                ...state,
-                loading: false
-            }
-        case USER_LOADED_SUCCESS:
-            return {
-                ...state,
-                user: payload
-            }
-        case USER_LOADED_FAIL:
-            return {
-                ...state,
-                user: null
-            }
-        case AUTHENTICATED_SUCCESS:
-            return {
-                ...state,
-                isAuthenticated: true
-            }
-        case AUTHENTICATED_FAIL:
-            localStorage.removeItem('access');
-            localStorage.removeItem('refresh');
-            return {
-                ...state,
-                isAuthenticated: false,
-                access: null,
-                refresh: null
-            }
-        case LOGIN_SUCCESS:
-            localStorage.setItem('access', payload.access);
-            localStorage.setItem('refresh', payload.refresh);
-            return {
-                ...state,
-                isAuthenticated: true,
-                access: localStorage.getItem('access'),
-                refresh: localStorage.getItem('refresh')
-            }
-
-        case ACTIVATION_SUCCESS:
-        case ACTIVATION_FAIL:
-        case RESET_PASSWORD_SUCCESS:
-        case RESET_PASSWORD_FAIL:
-        case RESET_PASSWORD_CONFIRM_SUCCESS:
-        case RESET_PASSWORD_CONFIRM_FAIL:
-            return{
-                ...state
-            }
-
-        case REFRESH_SUCCESS:
-            localStorage.setItem('access', payload.access);
-            return {
-                ...state,
-                access: localStorage.getItem('access')
-            }
-
+        
         case SIGNUP_SUCCESS:
+
         case SIGNUP_FAIL:
-        case LOGIN_FAIL:
-        case REFRESH_FAIL:
-        case LOGOUT:
             localStorage.removeItem('access')
             localStorage.removeItem('refresh')
-            return {
+            return{
                 ...state,
-                access :null,
+                access:null,
                 refresh:null,
-                isAuthenticated: false,
-                user: null,
+                isAuthenticated:false,
+                user:null,
             }
         default:
             return state
