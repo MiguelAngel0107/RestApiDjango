c31b499af0568e07fa2378ac7978ebabbe916ff7
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2023-01-02 18:41:03 -0500

    update

--- RestApi/src/App.js
+++ RestApi/src/App.js
@@ -8,9 +8,13 @@ import Error404 from "./containers/errors/eror404";
 import Signup from './containers/auth/singup';
 import Login from './containers/auth/login';
 import Activate from './containers/auth/activate';
+
 import ResetPassword from './containers/auth/resetPassword';
 import ResetPasswordConfirm from './containers/auth/resetPasswordConfirm';
+
 import Shop from './containers/Shop'
+import ProductDetail from './containers/pages/productDetail';
+
 
 function App() {
   return (
@@ -25,10 +29,12 @@ function App() {
           <Route exact path='/signup' element={<Signup/>}/>
           <Route exact path='/login' element={<Login/>}/>
           <Route exact path='/activate/:uid/:token' element={<Activate/>}/>
+
           <Route exact path='/reset_password' element={<ResetPassword/>}/>
           <Route exact path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm/>}/>
-          <Route exact path='/shop' element={<Shop/>}/>
 
+          <Route exact path='/shop' element={<Shop/>}/>
+          <Route exact path='/product/:productId' element={<ProductDetail/>}/>
 
         </Routes>
       </Router>
